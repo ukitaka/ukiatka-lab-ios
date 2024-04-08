6d0789fb2e30909b6f6a007d83ba8e25543e6302
ukitaka <yuki.takahashi.1126@gmail.com>
2024-04-08 17:45:09 +0900

    Add project

---
+++ Lab/LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabApp.swift
+//  Lab
+//
+//  Created by Yuki Takahashi on 2024/04/08.
+//
+
+import SwiftUI
+
+@main
+struct LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
