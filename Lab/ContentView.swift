dd0daf6743ea911407c4397ad5d8f3e9d81e64cc
ukitaka <yuki.takahashi.1126@gmail.com>
2024-04-09 19:07:13 +0900

    Add feature

--- Lab/ContentView.swift
+++ Lab/ContentView.swift
@@ -1,14 +1,41 @@
 import SwiftUI
 
+struct Feature: Identifiable {
+    var id: String
+    let name: String
+    
+    var code: String {
+        """
+        code **block** _is_
+        
+        
+        ```swift
+        print("hello world")
+        ```
+        """
+    }
+}
+
+let features: [Feature] = [
+    Feature(id: "A", name: "AAA"),
+    Feature(id: "B", name: "BBB"),
+    Feature(id: "C", name: "CCC"),
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationSplitView {
+            List(features) { item in
+                NavigationLink {
+                    let attrString = try? AttributedString(markdown: item.code)
+                    Text(attrString ?? "error")
+                } label: {
+                    Text(item.name)
+                }
+            }
+        } detail: {
+            Text("detail")
         }
-        .padding()
     }
 }
 
