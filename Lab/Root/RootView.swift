23e5ab5e22eb5aa2ee31408de32fddf15966860a
ukitaka <yuki.takahashi.1126@gmail.com>
2024-04-12 12:41:35 +0900

    Add fadeIn animation

--- Lab/Root/RootView.swift
+++ Lab/Root/RootView.swift
@@ -5,6 +5,8 @@ struct RootView: View {
     @Bindable var store: StoreOf<RootFeature>
 
     @State private var rotationDegrees = 0.0
+    @State private var opacity = 0.0
+
     private var animation: Animation {
         .easeInOut
             .speed(0.3)
@@ -25,9 +27,13 @@ struct RootView: View {
                     .resizable()
                     .frame(width: 64, height: 64)
                     .rotationEffect(.degrees(rotationDegrees))
+                    .opacity(opacity)
                     .onAppear {
                         withAnimation(animation) {
                             rotationDegrees = 360.0
+                        }
+                        withAnimation(.linear(duration: 0.1)) {
+                            opacity = 1.0
                         }
                     }
             }
