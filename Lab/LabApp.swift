d9126249a48b48c2e3afcd2fbe69fe0e91cf00f4
ukitaka <yuki.takahashi.1126@gmail.com>
2024-04-12 18:21:32 +0900

    Remove unused client

--- Lab/LabApp.swift
+++ Lab/LabApp.swift
@@ -2,11 +2,6 @@ import ComposableArchitecture
 import Supabase
 import SwiftUI
 
-let client = SupabaseClient(
-    supabaseURL: URL(string: "https://xwyamkobysyldtpyljfm.supabase.co")!,
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3eWFta29ieXN5bGR0cHlsamZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA5OTY1OTUsImV4cCI6MjAyNjU3MjU5NX0.5KeUbSWo9bQ0V5RHMx8ocUxBfA9swjMflEWRg7XiUXo"
-)
-
 @main
 struct LabApp: App {
     var body: some Scene {
