aca1da23add4523b67177473c6bdf37cf3fe6e65
ukitaka <yuki.takahashi.1126@gmail.com>
2024-04-14 17:07:17 +0900

    Show bookmarks

--- Lab/Features/Home/HomeView.swift
+++ Lab/Features/Home/HomeView.swift
@@ -9,13 +9,27 @@ struct HomeView: View {
         switch store.state {
         case let .bookmarks(bookmarks):
             NavigationStack {
-                List(bookmarks) { bookmark in
-                    NavigationLink(bookmark.title, value: bookmark)
-                }
-                .navigationDestination(for: Bookmark.self) { bookmark in
-                    Text(bookmark.title)
-                }
-            }.navigationTitle("ブックマーク")
+                ScrollView {
+                    ForEach(bookmarks) { bookmark in
+                        if let imageUrl = bookmark.imageUrl {
+                            AsyncImage(url: URL(string: imageUrl)) { image in
+                                image.resizable()
+                                    .frame(maxWidth: .infinity)
+                                    .scaledToFit()
+                            } placeholder: {
+                                LogoLoadingView(width: 32.0, height: 32.0)
+                            }
+                            Spacer()
+                        } else {
+                            //                        Text(bookmark.title)
+                            Spacer()
+                        }
+                    }
+                    .navigationDestination(for: Bookmark.self) { bookmark in
+                        Text(bookmark.title)
+                    }
+                }.navigationTitle("ブックマーク")
+            }
 
         case .fetching:
             LogoLoadingView(width: 32.0, height: 32.0)
