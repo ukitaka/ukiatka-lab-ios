a643ab54773b6e2464370db4d3cabfd78102b80d
ukitaka <yuki.takahashi.1126@gmail.com>
2024-04-17 11:20:21 +0900

    Use custom UI

--- Extensions/ShareExtension/ShareViewController.swift
+++ Extensions/ShareExtension/ShareViewController.swift
@@ -1,35 +1,32 @@
-import Lab
 import Social
 import UIKit
 import UniformTypeIdentifiers
 
-class ShareViewController: SLComposeServiceViewController {
-    override func presentationAnimationDidFinish() {
-        super.presentationAnimationDidFinish()
+class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         guard let extensionContext else {
-            return cancel()
+            return
         }
 
         guard let extensionItem = extensionContext.inputItems.first as? NSExtensionItem else {
-            return cancel()
+            return
         }
 
         guard let itemProvider = extensionItem.attachments?.first else {
-            return cancel()
+            return
         }
 
         guard itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier) else {
-            return cancel()
+            return
         }
 
         itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier, options: nil) { item, _ in
             guard let url = item as? URL else { return }
             print(url)
-            extensionContext.completeRequest(returningItems: [], completionHandler: { _ in
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
-                    self.cancel()
-                }
-            })
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.2) {
+                extensionContext.completeRequest(returningItems: [], completionHandler: nil)
+            }
         }
         // extensionContext.cancelRequest(withError: error)
     }
