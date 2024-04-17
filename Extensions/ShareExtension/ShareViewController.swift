9575e669c2d6e2d4a81c14f0db0c388200c2e5d0
ukitaka <yuki.takahashi.1126@gmail.com>
2024-04-17 12:09:59 +0900

    tried to open custom url scheme but failed

--- Extensions/ShareExtension/ShareViewController.swift
+++ Extensions/ShareExtension/ShareViewController.swift
@@ -5,29 +5,41 @@ import UniformTypeIdentifiers
 class ShareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let extensionContext else {
-            return
-        }
 
-        guard let extensionItem = extensionContext.inputItems.first as? NSExtensionItem else {
-            return
+        guard let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem else {
+            return close()
         }
 
         guard let itemProvider = extensionItem.attachments?.first else {
-            return
+            return close()
         }
 
         guard itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier) else {
-            return
+            return close()
         }
 
         itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier, options: nil) { item, _ in
-            guard let url = item as? URL else { return }
-            print(url)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5.2) {
-                extensionContext.completeRequest(returningItems: [], completionHandler: nil)
+            guard let url = item as? URL else {
+                return self.close()
+            }
+            var uc = URLComponents()
+            uc.scheme = "app.ukitaka-lab"
+            uc.host = "ukitaka-lab"
+            uc.path = "/add_bookmarks"
+            uc.queryItems = [URLQueryItem(name: "url", value: url.absoluteString)]
+            print(uc.url)
+
+            DispatchQueue.main.async {
+                self.extensionContext?.completeRequest(returningItems: []) { _ in
+                    self.extensionContext?.open(uc.url!)
+                }
             }
         }
+
         // extensionContext.cancelRequest(withError: error)
+    }
+
+    func close() {
+        extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
 }
