48145a41bb509f4cb99f3e1b193725c5f73e9080
ukitaka <yuki.takahashi.1126@gmail.com>
2024-04-18 14:20:01 +0900

    Loading and success message

--- Extensions/ShareExtension/ShareView.swift
+++ Extensions/ShareExtension/ShareView.swift
@@ -4,10 +4,47 @@ import SwiftUI
 struct ShareView: View {
     @Bindable var store: StoreOf<ShareFeature>
 
+    @State var opacity: CGFloat = 0.0
+
     @ViewBuilder
     var body: some View {
-        VStack {
-            LogoLoadingView(width: 32.0, height: 32.0)
+        VStack(spacing: 16.0) {
+            switch store.state {
+            case .adding:
+                LogoLoadingView(width: 32.0, height: 32.0)
+                Text("ブックマークに追加しています....")
+                    .font(.caption)
+
+            case .success:
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 64.0, height: 64.0)
+                    .foregroundColor(.labPrimary)
+                    .opacity(opacity)
+                    .onAppear {
+                        withAnimation(.linear(duration: 0.1)) {
+                            opacity = 1.0
+                        }
+                    }
+                Text("ブックマークに追加しました！")
+                    .fontWeight(.semibold)
+                    .opacity(opacity)
+                    .onAppear {
+                        withAnimation(.linear(duration: 0.1)) {
+                            opacity = 1.0
+                        }
+                    }
+
+            case .failure:
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 64.0, height: 64.0)
+                    .foregroundColor(.red)
+                Text("失敗しました")
+                    .fontWeight(.semibold)
+            }
         }
         .onAppear {
             store.send(.startAddBookmarkRequest)
