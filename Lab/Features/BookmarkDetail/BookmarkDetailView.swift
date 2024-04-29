dcd3ad9d9376e5bf6083a452089fa272965eeb27
ukitaka <yuki.takahashi.1126@gmail.com>
2024-04-29 17:24:06 +0900

    Add delete button

--- Lab/Features/BookmarkDetail/BookmarkDetailView.swift
+++ Lab/Features/BookmarkDetail/BookmarkDetailView.swift
@@ -15,65 +15,28 @@ struct BookmarkDetailView: View {
         ScrollView {
             VStack(alignment: .leading) {
                 BookmarkImage(bookmark: store.bookmark)
-                Divider()
-                    .padding([.top, .bottom], 8.0)
+                Divider().padding([.top, .bottom], 8.0)
+                siteNameAndTitle()
+                Divider().padding([.top, .bottom], 8.0)
+                llmSummarySection().padding(4.0)
+                Divider().padding([.top, .bottom], 8.0)
                 HStack(alignment: .center) {
-                    AsyncImage(url: store.bookmark.favicon) { image in
-                        image.resizable()
-                            .frame(width: 16.0, height: 16.0)
-                            .scaledToFit()
-                    } placeholder: {
-                        Skeleton()
-                            .frame(width: 16.0, height: 16.0)
-                    }
-                    Text(store.bookmark.siteNameForDisplay)
-                        .font(.caption)
-                        .foregroundStyle(.labText)
-                    Spacer()
-                    Text(dateFormatter.string(from: store.bookmark.createdAt))
-                        .font(.caption)
-                        .foregroundStyle(.labText)
-                    if store.isFetching {
-                        LogoLoadingView(width: 16.0, height: 16.0)
-                    }
-                }.padding(.bottom, 4.0)
-                Text(store.bookmark.title)
-                    .font(.headline)
-                    .padding(.bottom, 16.0)
-                Divider().padding([.top, .bottom], 8.0)
-                VStack(alignment: .leading) {
-                    HStack(alignment: .center) {
-                        Image(systemName: "pencil.and.scribble")
-                            .foregroundColor(.labText)
-                        Text("AI要約")
-                            .foregroundColor(.labText)
-                            .fontWeight(.semibold)
-                    }
-                    .padding(.bottom, 8.0)
-                    if let llmSummary = store.bookmark.llmSummary {
-                        switch llmSummary.status {
-                        case .completed:
-                            Text(llmSummary.summary).lineSpacing(8.0)
-
-                        case .queued:
-                            Text("要約を生成中です...").lineSpacing(8.0)
-                        }
-                    } else {
-                        HStack {
-                            Button("要約をリクエスト", systemImage: "pencil.and.scribble") {
-                                store.send(.requestLLMSummary)
-                            }
-                            .disabled(store.isFetching)
-                            .frame(width: 180.0, height: 48.0)
-                            .background(store.isFetching ? .gray : Color.labPrimary)
-                            .foregroundColor(Color.white)
-                            .cornerRadius(16.0)
-                            .fontWeight(.semibold)
-                        }
-                    }
+                    Image(systemName: "gearshape.fill")
+                        .foregroundColor(.labText)
+                    Text("ブックマーク管理")
+                        .foregroundColor(.labText)
+                        .fontWeight(.semibold)
                 }
-                .padding(4.0)
-                Divider().padding([.top, .bottom], 8.0)
+                .padding(.bottom, 8.0)
+                Button("ブックマークを削除", systemImage: "trash") {
+                    print("delete!")
+                }
+                .disabled(store.isFetching)
+                .frame(width: 180.0, height: 48.0)
+                .background(store.isFetching ? .gray : Color.labDanger)
+                .foregroundColor(Color.white)
+                .cornerRadius(16.0)
+                .fontWeight(.semibold)
                 Spacer()
             }
             .padding()
@@ -84,6 +47,68 @@ struct BookmarkDetailView: View {
         }
         .onAppear {
             store.send(.refetchBookmarkDetail)
+        }
+    }
+
+    @ViewBuilder
+    func siteNameAndTitle() -> some View {
+        HStack(alignment: .center) {
+            AsyncImage(url: store.bookmark.favicon) { image in
+                image.resizable()
+                    .frame(width: 16.0, height: 16.0)
+                    .scaledToFit()
+            } placeholder: {
+                Skeleton()
+                    .frame(width: 16.0, height: 16.0)
+            }
+            Text(store.bookmark.siteNameForDisplay)
+                .font(.caption)
+                .foregroundStyle(.labText)
+            Spacer()
+            Text(dateFormatter.string(from: store.bookmark.createdAt))
+                .font(.caption)
+                .foregroundStyle(.labText)
+            if store.isFetching {
+                LogoLoadingView(width: 16.0, height: 16.0)
+            }
+        }.padding(.bottom, 4.0)
+        Text(store.bookmark.title)
+            .font(.headline)
+            .padding(.bottom, 16.0)
+    }
+
+    @ViewBuilder
+    func llmSummarySection() -> some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                Image(systemName: "pencil.and.scribble")
+                    .foregroundColor(.labText)
+                Text("AI要約")
+                    .foregroundColor(.labText)
+                    .fontWeight(.semibold)
+            }
+            .padding(.bottom, 8.0)
+            if let llmSummary = store.bookmark.llmSummary {
+                switch llmSummary.status {
+                case .completed:
+                    Text(llmSummary.summary).lineSpacing(8.0)
+
+                case .queued:
+                    Text("要約を生成中です...").lineSpacing(8.0)
+                }
+            } else {
+                HStack {
+                    Button("要約をリクエスト", systemImage: "pencil.and.scribble") {
+                        store.send(.requestLLMSummary)
+                    }
+                    .disabled(store.isFetching)
+                    .frame(width: 180.0, height: 48.0)
+                    .background(store.isFetching ? .gray : Color.labPrimary)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(16.0)
+                    .fontWeight(.semibold)
+                }
+            }
         }
     }
 }
